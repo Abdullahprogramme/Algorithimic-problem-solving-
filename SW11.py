6eefd8783ab00395773083b9484cfb3c8939bb8a
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2023-10-31 18:26:29 +0500

    Update SW11.py

--- SW11.py
+++ SW11.py
@@ -28,3 +28,12 @@ def facht2(A):
     else: return A % 10 + facht2(A // 10)
 
 print(facht2(6))
+
+def binary_string(n):
+    if n == 1: return '1'
+    elif n == 0: return '0'
+    else:
+        if n < 0: return '-' + binary_string(-n)
+        else: return binary_string(n // 2) + str(n % 2)
+        
+print(binary_string(-40))
